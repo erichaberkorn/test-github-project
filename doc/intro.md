e89c2998281e6e77870f747cecfc1a7a3ddfbaa6
Matthew Boston <matthew@matthewboston.com>
2016-09-20 17:05:58 -0600

    add a test leiningen project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to test-github-project
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/test_github_project/core.clj
@@ -0,0 +1,7 @@
+(ns test-github-project.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
