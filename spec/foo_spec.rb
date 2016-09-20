6b8b7ec5ff0be18d8923170143bc4d4272b5465b
Matthew Boston <matthew@matthewboston.com>
2016-09-20 10:56:16 -0600

    add more testing examples

--- spec/foo_spec.rb
+++ spec/foo_spec.rb
@@ -1,7 +1,25 @@
 require "foo"
 
-RSpec.describe Foo, "#fail" do
+RSpec.describe Foo, "#foo" do
+  it "passes" do
+    expect(true).to be true
+  end
+
 	it "fails" do
-    expect(false).to be_true
+    expect(false).to be true
+  end
+
+	it "also fails" do
+    expect(false).to be true
+  end
+
+  # it "errors" do
+  #   raise "error"
+  # end
+
+  it "skips"
+
+  it "is pending" do
+    pending "TODO"
   end
 end
