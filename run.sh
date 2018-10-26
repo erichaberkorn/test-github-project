d1bb69f3db2a0fc842d835f5966e78f92bdbf2b8
Eric <eric@haberkorn.co>
2018-10-26 12:54:32 -0400

    log more stuff

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-for i in `seq 1 400000`;
+for i in `seq 1 2000000`;
 do
-    echo $i
+    echo "$i I am logging lots and lots of stuff not just numbers now"
 done
 
 
-for i in `seq 400001 1000000`;
+for i in `seq 20000001 21000000`;
 do
     sleep 1
     echo $i
